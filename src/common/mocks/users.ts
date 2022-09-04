0350b6ae72d8c5e4c2bf42d123867f9441f779ad
Takhyun Kim <kimkih1218@gmail.com>
2022-09-04 18:38:59 +0900

    feat: add users mock data and User type

---
+++ src/common/mocks/users.ts
@@ -0,0 +1,18 @@
+import type { User } from "../types/user";
+
+export const users: User[] = [
+  {
+    id: "1",
+    name: "takhyunKim",
+    country: "Korea",
+    birthday: new Date("1997-11-26"),
+    age: 26,
+  },
+  {
+    id: "2",
+    name: "taehyunKim",
+    country: "Korea",
+    birthday: new Date("2001-01-14"),
+    age: 22,
+  },
+];
